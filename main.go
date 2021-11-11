9aa531eca5875c1971047c0ec41dad92805ffbb8
Fakhrii Habibillah An Naufal <fha.naufal06@gmail.com>
2021-11-11 09:07:16 +0700

    added feature B

--- main.go
+++ main.go
@@ -8,8 +8,15 @@ func main() {
 
 	// use feature A
 	featureA("mango")
+
+	// use feature B
+	fmt.Println(featureB(2, 3))
 }
 
 func featureA(str string) {
 	fmt.Println(str)
+}
+
+func featureB(a, b int64) int64 {
+	return a + b
 }
